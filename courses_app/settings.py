669212a554382455250e05f592a92e9a5b3f2873
Mintimir <kachaev.666@gmail.com>
2021-06-16 16:24:31 +0600

    rewrited staticfiles_storage

--- courses_app/settings.py
+++ courses_app/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
